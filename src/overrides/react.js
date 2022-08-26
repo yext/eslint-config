ef2e09cc78ca10523386a966bfa959816ec47f74
Yen Truong <ytruong@yext.com>
2022-08-26 12:50:59 -0400

    also turn off require-default-props

--- src/overrides/react.js
+++ src/overrides/react.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     'react/jsx-props-no-spreading': 'off',
-    'react/function-component-definition': 'off'
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off'
   }
 };
