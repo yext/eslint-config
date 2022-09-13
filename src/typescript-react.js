6fd77e8e85015bbaf583617ef5952eeabae6bd36
nmanu1 <nmanu@yext.com>
2022-09-13 12:01:37 -0400

    Update TSX config

--- src/typescript-react.js
+++ src/typescript-react.js
@@ -1,14 +1,27 @@
 module.exports = {
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "./overrides/javascript", // override base rules in airbnb + add new rules
-    "./overrides/typescript", // override rules in airbnb-typescript + add new rules
-    "./overrides/react", // override react rules in airbnb + add new rules
+    "react-app",
+    "react-app/jest",
+    "plugin:react-perf/recommended",
+    "@yext/eslint-config",
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    project: "./tsconfig.json",
+  rules: {
+    "react/jsx-curly-brace-presence": ["warn", "never"],
+    "react-perf/jsx-no-new-object-as-prop": 1,
+    "react-perf/jsx-no-new-array-as-prop": 1,
+    "react-perf/jsx-no-new-function-as-prop": 1,
+    "testing-library/prefer-screen-queries": 1,
+    "testing-library/no-unnecessary-act": 1,
+    "jest/no-conditional-expect": 1,
+    "import/first": 1,
+
+    // These code formatting rules are inherited from eslint-config-react-app
+    // and are explicitly turned off to avoid conflicts with Prettier
+    "dot-location": "off",
+    "new-parens": "off",
+    "no-whitespace-before-property": "off",
+    "rest-spread-spacing": "off",
+    "unicode-bom": "off",
+    "no-mixed-operators": "off",
   },
 };
